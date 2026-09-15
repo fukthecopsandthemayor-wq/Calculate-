efcd38b817a4b5e0fb8e001f1179184c9799e966
fukthecopsandthemayor-wq <fukthecopsandthemayor@gmail.com>
2026-09-15 18:21:11 -0400

    Mount separate AI question scanner

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
+import AIQuestionScan from './AIQuestionScan.jsx';
 import './styles.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
+    <AIQuestionScan />
   </React.StrictMode>,
 );
